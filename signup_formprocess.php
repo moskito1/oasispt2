4e4862d03ebb024dce5d48b23ba1836ca4ce780b
dfrimpillo <rimpillodesserie@gmail.com>
2023-10-07 22:45:16 +0800

    added bunch of functions for validation

--- signup_formprocess.php
+++ signup_formprocess.php
@@ -1,30 +1,40 @@
 <?php
-    session_start();
-    include('dbconnection.php');
-    if (isset($_POST['signup'])) {
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $password_hash = password_hash($password, PASSWORD_BCRYPT);
-        $query = $connection->prepare("SELECT * FROM userInfo WHERE email=:email");
-        $query->bindParam("email", $email, PDO::PARAM_STR);
-        $query->execute();
-        if ($query->rowCount() > 0) {
-            echo '<p class="error">The email address is already registered!</p>';
-        }
-        if ($query->rowCount() == 0) {
-            $query = $connection->prepare("INSERT INTO userInfo(username,password, email) 
-                                        VALUES (:username,:password_hash,:email)");
-            $query->bindParam("username", $username, PDO::PARAM_STR);
-            $query->bindParam("password_hash", $password_hash, PDO::PARAM_STR);
-            $query->bindParam("email", $email, PDO::PARAM_STR);
 
-            $result = $query->execute();
-            if ($result) {
-                header("location: login.php");
-            } else {
-                echo '<p class="error">Something went wrong!</p>';
-            }
+    if (isset($_POST["submit"])){
+
+        $firstName = $_POST["firstname"];
+        $lastName = $_POST["lastname"];
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $passwordRepeat = $_POST["passwordRepeat"];
+
+        require_once 'dbconnection.php';
+        require_once 'functions.php';
+        
+        if (emptyInputSignup($firstName, $lastName, $username, $email, $password, $passwordRepeat) !== false){
+            header("location: signup.php?error=emptyinput");
+            exit();
+        } 
+        if (invalidUsername($username) !== false){
+            header("location: signup.php?error=invalidusername");
+            exit();
+        } 
+        if (invalidEmail($email) !== false){
+            header("location: signup.php?error=invalidemail");
+            exit();
+        } 
+        if (passwordMatch($password, $passwordRepeat) !== false){
+            header("location: signup.php?error=passwordsdontmatch");
+            exit();
         }
+        if (usernameExists($conn, $username, $email) !== false){
+            header("location: signup.php?error=usernametaken");
+            exit();
+    } 
+
+    createUser($conn,$firstName, $lastName, $username, $email, $password, $passwordRepeat);
+    else {
+        header("location: signup.php");
+        exit();
     }
-?>
