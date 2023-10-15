bb731d1531f1051025eb3a741d20bd868901734e
moskito1 <147070342+moskito1@users.noreply.github.com>
2023-10-15 19:31:16 +0800

    Create cart_record.sql

---
+++ cart_record.sql
@@ -0,0 +1,8 @@
+CREATE TABLE record
+  (id BIGINT(50) NOT NULL,
+  Item_Name VARCHAR(255) NOT NULL,
+  size_oz VARCHAR(255) NOT NULL,
+  price DOUBLE(10,2) NOT NULL,
+  quantity BIGINT(50) NOT NULL,
+  total_price DOUBLE(10,2)
+  );
