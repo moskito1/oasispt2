858106351c1c3ecd324412e1768ab407a417d1be
dfrimpillo <rimpillodesserie@gmail.com>
2023-10-26 21:21:54 +0800

    footer

--- aboutus.php
+++ aboutus.php
@@ -61,46 +61,7 @@ Join us on this journey for perfect coffee, where every cup is a taste of calmne
       </div>  
 
 
-<div class="footer">
-  <div class="first-column">
-    <h4>FOR MORE INQUIRIES</h4>
-    <div class="first-column-child">
-      <img src="icons" alt="" >
-      <p>
-      Near Pyur Luxury, Ascott, 5th Avenue Corner <br> 28th Street, Bonifactio Global City, Metro Manila </p>
-    </div>
-    <div class="first-column-child">
-      <img src="icons" alt="">
-      <p>
-      +639532913412</p>
-    </div>
-    <div class="first-column-child">
-      <img src="icons" alt="">
-      <p>
-      oasiscafe@gmail.com</p>
-    </div>
+      <?php include "footer.php" ?>
 
-  </div>
-  <div class="second-column">
-    <h4>FOLLOW US</h4>
-    <p>Find us on our <br>social media accounts!</p>
-    <div class="second-column-child">
-      <a href="https://www.facebook.com/profile.php?id=100092202248783&mibextid=ZbWKwL"><img src="icons" alt=""></a>
-      <a href="https://instagram.com/oasiscafe647?igshid=ZGUzMzM3NWJiOQ=="><img src="icons" alt=""></a>
-      <a href=""><img src="icons" alt=""></a> 
-
-    </div>
-
-  </div>
-
-  <div class="third-column">
-    <h4>OPEN HOURS</h4>
-    <p style="font-weight: bold;">Monday to Friday</p>
-    <p>6:00 AM TO 11:00 PM</p>
-    <p style="font-weight: bold;">Saturday to Sunday</p>
-    <p>8:00 AM TO 1:00 AM</p>
-  </div>
-</div> 
-</div>
     </body>
 </html>
---
+++ footer.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<head>
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+<div class="footer">
+  <div class="first-column">
+    <h4>FOR MORE INQUIRIES</h4>
+    <div class="first-column-child">
+    <i class=" fa fa-map-marker"></i>
+          <p>Near Pyur Luxury, Ascott, 5th Avenue Corner <br> 
+      28th Street, Bonifactio Global City, Metro Manila </p>
+    </div>
+    <div class="first-column-child">
+      <i class="fa fa-solid fa-phone"></i>
+      <p>+639532913412</p>
+    </div>
+    <div class="first-column-child">
+      <i class="fa fa-solid fa-envelope"></i>
+      <p>oasiscafe@gmail.com</p>
+    </div>
+
+  </div>
+  <div class="second-column">
+    <h4>FOLLOW US</h4>
+    <p>Find us on our <br>social media accounts!</p>
+    <div class="second-column-child">
+      <a href="https://www.facebook.com/profile.php?id=100092202248783&mibextid=ZbWKwL"><i class="fa fa-brands fa-facebook"></i></a>
+      <a href="https://instagram.com/oasiscafe647?igshid=ZGUzMzM3NWJiOQ=="><i class="fa fa-brands fa-instagram"></i></a>
+
+    </div>
+
+  </div>
+
+  <div class="third-column">
+    <h4>OPEN HOURS</h4>
+    <p style="font-weight: bold;">Monday to Friday</p>
+    <p>6:00 AM TO 11:00 PM</p>
+    <p style="font-weight: bold;">Saturday to Sunday</p>
+    <p>8:00 AM TO 1:00 AM</p>
+  </div>
+</div> 
+</div>
+</body>
+</html>
